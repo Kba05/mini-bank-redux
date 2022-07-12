0418eb106be627b56b14363861688f0913f2a299
Babakan <babakhan.kurbanov@saprun.com>
2022-07-12 14:01:42 +0300

    done redux lesson

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { fetchCustomers } from "./asyncActions/customers"
+import { addCustomerAction, removeCustomerAction } from "./store/customersReducer"
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch()
+  const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customers.customers)
+
+  const onClickAdd = ()=>{
+    dispatch({type:"ADD_CASH", payload:2})
+  }
+  const onClickGet = ()=>{
+    dispatch({type:"GET_CASH", payload:2})
+  }
+
+  const onClickGetCustomer = ()=>{
+    dispatch(fetchCustomers())
+  }
+
+  const onClickAddCustomer = (name)=>{
+    const obj = {
+      name,
+      id: Date.now()
+    }
+    dispatch(addCustomerAction(obj))
+  }
+
+  const onClickRemoveCustomer = (name)=>{
+    dispatch(removeCustomerAction(name))
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <p>Your cash: {cash} </p>
+        <button onClick={onClickAdd}>Add cash</button>
+        <button onClick={onClickGet}>Get cash</button>
+      </div>
+
+      <div>
+        <button onClick={()=>onClickGetCustomer()}>Get customer</button>
+        <button onClick={()=>onClickAddCustomer(prompt())}>Add customer</button>
+        <button onClick={()=>onClickRemoveCustomer(prompt())}>Remove customer</button>
+        <p>customers:</p>
+        {customers.map(customer=> <p key={customer.id}>{customer.name}</p> )}
+      </div>
     </div>
   );
 }
